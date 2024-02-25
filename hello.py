8170abb4431d40bddc5e1ec1cc6f93daf1810291
Alex <pedrolop15@hotmail.com>
2024-02-25 01:16:43 -0600

    refactor: update hello app
    Update hello app to show return examples

--- hello.py
+++ hello.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/version")
+def exampleDict():
+    return {"name":"flask", "version":3}
+
+@app.route("/test")
+def test():
+    return "Exito",201
+
+@app.route("/headers")
+def headers():
+    return {"status":False},404,{"custom": "flask"}
